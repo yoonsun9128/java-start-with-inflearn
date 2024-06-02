93df0e13ea5fbfe2325767c65a72c8679d36d24b
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-06-02 16:17:24 +0900

    comment use

---
+++ src/Comment.java
@@ -0,0 +1,12 @@
+public class Comment {
+//psvm -> public static void main 이 자동으로 생성된다.
+    public static void main(String[] args) {
+//      sout -> System.out.println 자동완성
+        System.out.println("hello java1"); //hello java1을 출력합니다.
+//        System.out.println("hello java2");
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+//    maind을 시작으로 위에서 아래로 한 줄씩 실행된다.
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java2");
+    }
+}
