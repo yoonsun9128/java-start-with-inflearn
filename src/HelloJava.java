40648b9699a7701df6ef234757673c4153cf0674
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-06-02 16:07:09 +0900

    자바 프로그램 실행

---
+++ src/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+//    프로그램을 시작시키는 시작점 이라고 생각하면 된다.
+//    자바 언어는 대소문자를 구분하다! 꼭 대소문자를 잘 구분해서 작성하기!!
+//    { } => 블록 => class 또는 함수의 시작과 끝을 나타낸다.
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
