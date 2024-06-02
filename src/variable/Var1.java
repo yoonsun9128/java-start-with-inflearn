3b792b805b06a8f82eb76d138d75e83a9b58acea
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-06-02 16:55:12 +0900

    variable use

---
+++ src/variable/Var1.java
@@ -0,0 +1,10 @@
+//package 안에 만들었으면 꼭 어디 위치인지를 선언해줘야한다.
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
+    }
+}
