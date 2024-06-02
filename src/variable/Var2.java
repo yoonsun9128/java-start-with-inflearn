3815ee60b2d3b229b53f31cdec516442d3290c5f
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-06-02 17:00:12 +0900

    variable chagne

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -2,12 +2,14 @@ package variable;
 
 public class Var2 {
     public static void main(String[] args) {
-        int a; //변수 선언
+        int a; //변수 선언 => 변환이 되는 값
         a = 20; //변수 초기화
 
         System.out.println(a);
         System.out.println(a);
         System.out.println(a);
 
+        a = 40; //변수 값 변경
+        System.out.println(a);
     }
 }
