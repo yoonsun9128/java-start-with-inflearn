bfaea3dfba36db63327ee9a2a9bcbe004ddd3e00
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-06-03 21:33:57 +0900

    variable question

---
+++ src/variable/ex/Question3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class Question3 {
+    public static void main(String[] args) {
+         long longType = 10000000000000L;
+        System.out.println("longType = " + longType);
+         boolean booleanType = true ;
+        System.out.println("booleanType = " + booleanType);
+
+        System.out.println(longType);
+        System.out.println(booleanType);
+    }
+}
