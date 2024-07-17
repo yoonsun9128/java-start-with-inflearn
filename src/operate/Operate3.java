082fd8f86dbf7a96b2782184e3d4923942e21682
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-07-17 12:47:28 +0900

    연산자 우선순위

---
+++ src/operate/Operate3.java
@@ -0,0 +1,12 @@
+package operate;
+
+public class Operate3 {
+    public static void main(String[] args) {
+
+        //연산자 우선순위 : 곱셈 > 덧셈
+        int sum1 = 1 + 2 * 3; // 1 + (2*3)
+        int sum2 = (1 + 2) * 3;
+        System.out.println("sum1 = " + sum1);
+        System.out.println("sum2 = " + sum2);
+    }
+}
