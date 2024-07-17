d1e96dbbabdd5a99b2afd203edfb828ac867b60a
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2024-07-17 13:10:43 +0900

    증감 연산자

---
+++ src/operate/OperateAdd1.java
@@ -0,0 +1,23 @@
+package operate;
+
+public class OperateAdd1 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        a = a + 1;
+        System.out.println("a = " + a); //1
+
+        a = a + 1;
+        System.out.println("a = " + a); //2
+
+        //증간 연산자 -- 를쓰면 감소
+        ++a;
+        System.out.println("a = " + a); //3
+
+        ++a;
+        System.out.println("a = " + a); //4
+
+        --a;
+        System.out.println("a = "+ a); //3
+    }
+}
