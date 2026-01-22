d9b95d5df355d8dde36afb8e514fb23dc36f522f
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-01-22 21:11:33 +0900

    2차원 배열 리팩토링 응용

--- src/array/ArrayDi4.java
+++ src/array/ArrayDi4.java
@@ -1,13 +1,18 @@
 package array;
 
-public class ArrayDi3 {
+public class ArrayDi4 {
 	public static void main(String[] args) {
 
 		//2*3 2차원 배열을 만든다.
-		int[][] arr = {
-			{1,2,3},
-			{4,5,6}
-		}; //행2, 열3
+		int[][] arr = new int[2][3];
+
+		int i = 1;
+
+		for (int row = 0; row < arr.length; row++) {
+			for (int column = 0; column < arr[row].length; column++) {
+				arr[row][column] = i++;
+			}
+		}
 
 		for (int row = 0; row < arr.length; row++) {
 			for (int column = 0; column < arr[row].length; column++) {
