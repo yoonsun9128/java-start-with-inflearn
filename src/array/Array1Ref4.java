a2a36a8fb57a40a2e1190a97e055f45d860cb25e
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-01-22 20:48:34 +0900

    배열 리펙토링 수정

--- src/array/Array1Ref4.java
+++ src/array/Array1Ref4.java
@@ -4,6 +4,7 @@ public class Array1Ref4 {
 	public static void main(String[] args) {
 
 		int[] students = {90, 80, 70, 60, 50}; //배열 생성과 초기화
+		// -> 주위할 부분 라인을 나누면 안된다. 타입 정보에 대해서 같은 라인 선상에 있어야함
 
 		for (int i =0; i < students.length; i++) {
 			//문자 더하기 숫자는 문자다!!! () 많이 중요함!!!
